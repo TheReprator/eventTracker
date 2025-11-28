2a34fd2e0afcd4a4264a886540f90ee00f848745
Deep Sandhya <deepsandhdeepsandhyaya@Deeps-MacBook-Air.local>
2025-11-28 10:09:02 +0530

    locale changed for tab

--- src/appConfiguration/navigation/TabsNavigator.tsx
+++ src/appConfiguration/navigation/TabsNavigator.tsx
@@ -3,18 +3,26 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import EventScreen from '@/features/event/EventScreen';
 import ProfileScreen from '@/features/profile/ProfileScreen';
 import { TAB_ROUTES, TabStackParamList } from '@/types/navigation';
+import { useAppLocale } from '../localization/LocaleContext';
 
 const Tab = createBottomTabNavigator<TabStackParamList>();
 
 export default function TabsNavigator() {
+  return <TabsWithLocale />;
+}
+
+function TabsWithLocale() {
+  const { t } = useAppLocale();
+
   return (
-    <Tab.Navigator 
+    <Tab.Navigator
       screenOptions={({ route }) => ({
-        headerShown: false})}>
-
-        <Tab.Screen name={TAB_ROUTES.Event} component={EventScreen} />
-        <Tab.Screen name={TAB_ROUTES.Profile} component={ProfileScreen} />
-
+        headerShown: false,
+        tabBarLabel: t(`rootTab.${route.name}`)
+      })}
+    >
+      <Tab.Screen name={TAB_ROUTES.Event} component={EventScreen} />
+      <Tab.Screen name={TAB_ROUTES.Profile} component={ProfileScreen} />
     </Tab.Navigator>
   );
 }
