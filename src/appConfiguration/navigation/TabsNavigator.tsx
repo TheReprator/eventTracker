e4d1f15fbfb2894a49b22b11247544abf0273fb6
Deep Sandhya <deepsandhdeepsandhyaya@Deeps-MacBook-Air.local>
2025-11-28 10:23:22 +0530

    tab icon add

--- src/appConfiguration/navigation/TabsNavigator.tsx
+++ src/appConfiguration/navigation/TabsNavigator.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import EventScreen from '@/features/event/EventScreen';
-import ProfileScreen from '@/features/profile/ProfileScreen';
-import { TAB_ROUTES, TabStackParamList } from '@/types/navigation';
-import { useAppLocale } from '../localization/LocaleContext';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import EventScreen from "@/features/event/EventScreen";
+import ProfileScreen from "@/features/profile/ProfileScreen";
+import { TAB_ROUTES, TabStackParamList } from "@/types/navigation";
+import { useAppLocale } from "@/appConfiguration/localization/LocaleContext";
+import Ionicons from "@react-native-vector-icons/ionicons";
+import { useAppTheme } from "../theme/ThemeContext";
+
+type IconName = React.ComponentProps<typeof Ionicons>["name"];
+
+const TAB_ICONS: Record<string, { active: IconName; inactive: IconName }> = {
+  Event: {
+    active: "calendar",
+    inactive: "calendar-outline",
+  },
+  Profile: {
+    active: "person",
+    inactive: "person-outline",
+  },
+};
 
 const Tab = createBottomTabNavigator<TabStackParamList>();
 
 export default function TabsNavigator() {
-  return <TabsWithLocale />;
+  return <TabsWithLocaleAndIcons />;
 }
 
-function TabsWithLocale() {
+function TabsWithLocaleAndIcons() {
   const { t } = useAppLocale();
+  const { isDark, theme } = useAppTheme();
 
   return (
     <Tab.Navigator
-      screenOptions={({ route }) => ({
-        headerShown: false,
-        tabBarLabel: t(`rootTab.${route.name}`)
-      })}
+      screenOptions={({ route }) => {
+        const icons = TAB_ICONS[route.name];
+
+        return {
+          headerShown: false,
+          tabBarLabel: t(`rootTab.${route.name}`),
+
+          tabBarIcon: ({ color, focused }) => {
+            const iconName = focused ? icons.active : icons.inactive;
+            return <Ionicons name={iconName} size={22} color={color} />;
+          },
+
+          tabBarActiveTintColor: theme.colors.primary,
+          tabBarInactiveTintColor: isDark ? "#888" : "#666",
+        };
+      }}
     >
       <Tab.Screen name={TAB_ROUTES.Event} component={EventScreen} />
       <Tab.Screen name={TAB_ROUTES.Profile} component={ProfileScreen} />
